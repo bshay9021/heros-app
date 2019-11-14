0f45023fadac3051746c8cce91f5a10fb7063bca
Brandon Shay <brandonshay@Brandons-MacBook-Pro.local>
2019-11-13 20:00:47 -0500

    Update

---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROS } from '../mock-heros';
+
+
+
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.css']
+})
+export class HerosComponent implements OnInit {
+
+  heros = HEROS;
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
